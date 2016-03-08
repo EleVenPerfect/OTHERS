460f017fd54eac80a368db7bbcf05ea351263b8d
ATIME <atime@atime.org.cn>
2016-03-08 18:04:17 +0800

    unit4

---
+++ Cpp/Unit04/作业2.cpp
@@ -0,0 +1,78 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class Screen
+{
+public:
+	Screen(int w,int h)
+	{
+		exitWhenInvalidScreen(w);
+		exitWhenInvalidScreen(h);
+		height =h;
+		width =w;
+		cout << "screen" << endl;
+	}
+	
+	Screen()
+	{
+		height =480;
+		width =640;
+		cout << "screen" << endl;
+	}
+	
+	int getWidth()
+	{
+		return width;
+	}
+	
+	int getHeight()
+	{
+		return height;
+	}
+	
+	int setWidth(int width)
+	{
+		exitWhenInvalidScreen(width);
+		Screen::width =width;
+	}
+	
+	int setHeight(int height)
+	{
+		exitWhenInvalidScreen(height);
+		Screen::height =height;
+	}
+	
+private:
+	int height;
+	int width; 
+	char exitWhenInvalidScreen(int num)
+	{
+		if(num<=0||num>1000)
+		{
+			cout << "invalid screen size"; 
+			exit(0);
+//			return 0;
+		}
+		return 1;
+	}  
+};
+
+int main() {
+  int width, height;
+  std::cin >> width >> height;
+  Screen screen1 (width, height);
+  Screen screen2;
+   
+  screen2.setWidth(800);
+  screen2.setHeight(600);
+   
+  std::cout << screen1.getWidth() << ' ' << screen1.getHeight() << std::endl;
+  std::cout << screen2.getWidth() << ' ' << screen2.getHeight();
+   
+#ifdef DEBUG
+  std::cin.get();
+#endif
+  return 0;
+}
