8c0a266ab90c5101e48fb1e1bb978b111a05d9d1
ATIME <atime@atime.org.cn>
2017-04-24 23:07:25 +0800

    学习qt

---
+++ QT/20140424/Dialog/Dialog.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-04-24T22:26:19
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Dialog
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        dialog.cpp
+
+HEADERS  += dialog.h
+
+FORMS    += dialog.ui
---
+++ QT/20170424/build-dialog-Desktop_Qt_5_4_0_MinGW_32bit-Debug/ui_dialog.h
@@ -0,0 +1,76 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_H
+#define UI_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QLabel *label_1;
+    QLabel *label_2;
+    QLabel *label_3;
+    QPushButton *pushButton;
+    QLineEdit *lineEdit;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName(QStringLiteral("Dialog"));
+        Dialog->resize(397, 248);
+        label_1 = new QLabel(Dialog);
+        label_1->setObjectName(QStringLiteral("label_1"));
+        label_1->setGeometry(QRect(70, 60, 54, 12));
+        label_2 = new QLabel(Dialog);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(70, 120, 54, 12));
+        label_3 = new QLabel(Dialog);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(210, 120, 54, 12));
+        pushButton = new QPushButton(Dialog);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(200, 180, 75, 23));
+        lineEdit = new QLineEdit(Dialog);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(180, 60, 113, 20));
+
+        retranslateUi(Dialog);
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QApplication::translate("Dialog", "Dialog", 0));
+        label_1->setText(QApplication::translate("Dialog", "\345\215\212\345\276\204\357\274\232", 0));
+        label_2->setText(QApplication::translate("Dialog", "\351\235\242\347\247\257\357\274\232", 0));
+        label_3->setText(QString());
+        pushButton->setText(QApplication::translate("Dialog", "\350\256\241\347\256\227", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_H
