f747ae4da61ab1aef822a9999ed2c566bcfc5b5f
ATIME <atime@atime.org.cn>
2017-06-22 11:02:34 +0800

    adc

---
+++ STM32F407/16.各类统计信息/6.8M晶振168MHzADC设置好换接口tft命令解析/User/BSP/lcd/bsp_lcd.c
@@ -0,0 +1,65 @@
+#include "./lcd/bsp_lcd.h"  
+
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK STM32F407开发板
+//DAC 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//创建日期:2014/5/6
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2014-2024
+//All rights reserved 
+////////////////////////////////////////////////////////////////////////////////// 	
+
+unsigned char TFT480800START   = 0xEE;
+										//发送起始标志printf("%c",TFT480800START);
+unsigned char TFT480800STOP[4]	= {0xFF,0xFC,0xFF,0xFF};
+unsigned char TFT480800STOPUSER[4]	= {0xFF,0xFC,0xFF,0xAE};
+										//发送终止标志printf("%c%c%c%c",TFT480800STOP[0],TFT480800STOP[1],TFT480800STOP[2],TFT480800STOP[3] );
+
+void LCD_valtage_show(void)
+{
+	unsigned short int i;	
+	unsigned short int iTemp;
+	unsigned char a[6] = {0xB1, 0x10, 0x00, 0x00, 0x00, 0x00};
+	unsigned char c[9] = {0xB1, 0x32, 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x02};
+	unsigned char d,e;
+	/* 打印采集数据 */
+	for (i = 0; i < CH_NUM; i++)
+	{
+			iTemp = s_volt[i];	/* uV  */
+			a[5] = i+1;
+			printf("%c",TFT480800START);
+			printf("%c%c%c%c%c%c", a[0],a[1],a[2],a[3],a[4],a[5]);
+			if (s_dat[i] < 0)
+			{
+						iTemp = -iTemp;
+						printf("-%d.%d%d%d", iTemp /1000, (iTemp%1000)/100, (iTemp%100)/10, iTemp%10);
+			}
+			else
+			{
+						printf("+%d.%d%d%d", iTemp /1000, (iTemp%1000)/100, (iTemp%100)/10, iTemp%10);                    
+			}
+			printf("%c%c%c%c",TFT480800STOP[0],TFT480800STOP[1],TFT480800STOP[2],TFT480800STOP[3] );
+	}
+	
+	printf("%c",TFT480800START);
+	printf("%c%c%c%c%c%c%c%c%c", c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7],c[8]);
+	d = ((s_dat[3]&0xff00)>>8);
+	e = (s_dat[3]&0xff);
+	printf("%c%c", d, e );
+	printf("%c%c%c%c",TFT480800STOP[0],TFT480800STOP[1],TFT480800STOP[2],TFT480800STOP[3] );
+	
+}
+
+//切换画面
+void LCD_screen_show(unsigned char num)
+{
+		unsigned char a[3] = { 0xB1, 0x00, 0x00};
+		printf("%c",TFT480800START);
+		a[2] += num;
+		printf("%c%c%c", a[0],a[1],a[2]);
+		printf("%c%c%c%c",TFT480800STOP[0],TFT480800STOP[1],TFT480800STOP[2],TFT480800STOP[3] );
+}
---
+++ STM32F407/21.TFT程序01/output/DefineID.h
@@ -0,0 +1,63 @@
+//创建画面boot的命令索引ID
+#define  _SCREEN_BOOT                                                          0
+
+//创建画面selectmode的命令索引ID
+#define  _SCREEN_SELECTMODE                                                    1
+
+//创建画面connected的命令索引ID
+#define  _SCREEN_CONNECTED                                                     2
+
+//创建画面offlinesetmode的命令索引ID
+#define  _SCREEN_OFFLINESETMODE                                                3
+
+//创建画面offlinesetnumber的命令索引ID
+#define  _SCREEN_OFFLINESETNUMBER                                              4
+
+//创建画面offlinecontrl的命令索引ID
+#define  _SCREEN_OFFLINECONTRL                                                 5
+
+//画面boot中动画控件Animation1使用的图片
+#define  _IMG_BOOT_ANIMATION1                                                  0
+
+#define  _ANIMATION_BOOT_ANIMATION1                                            1
+
+//画面selectmode的背景图片
+#define  _IMG_SELECTMODE                                                       1
+
+//全局图片状态选择1.jpg
+#define  _IMG_状态选择1_JPG                                                        1
+
+//全局图片状态选择2.jpg
+#define  _IMG_状态选择2_JPG                                                        2
+
+#define  _BTN_SELECTMODE_BUTTON1                                               1
+
+#define  _BTN_SELECTMODE_BUTTON2                                               2
+
+//画面connected的背景图片
+#define  _IMG_CONNECTED                                                        3
+
+//画面offlinesetmode的背景图片
+#define  _IMG_OFFLINESETMODE                                                   4
+
+//全局图片离线模式选择测量模式2.jpg
+#define  _IMG_离线模式选择测量模式2_JPG                                                  5
+
+#define  _BTN_OFFLINESETMODE_BUTTON1                                           1
+
+#define  _BTN_OFFLINESETMODE_BUTTON2                                           2
+
+#define  _BTN_OFFLINESETMODE_BUTTON3                                           3
+
+#define  _BTN_OFFLINESETMODE_BUTTON4                                           4
+
+#define  _BTN_OFFLINESETMODE_BUTTON5                                           5
+
+#define  _BTN_OFFLINESETMODE_BUTTON6                                           6
+
+//画面offlinesetnumber的背景图片
+#define  _IMG_OFFLINESETNUMBER                                                 6
+
+//画面offlinecontrl的背景图片
+#define  _IMG_OFFLINECONTRL                                                    7
+
