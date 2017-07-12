4ef0f5598cd790aa7ac011755844285261b539ff
ATIME <atime@atime.org.cn>
2017-07-12 21:19:09 +0800

    C#

---
+++ C#/20170706/串口实验/串口实验/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace 串口实验
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.配置文件ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据文件ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.配置文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存数据ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.状态设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.采样模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开配置ToolStripMenuItem,
+            this.保存配置ToolStripMenuItem,
+            this.保存数据ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开配置ToolStripMenuItem
+            // 
+            this.打开配置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.配置文件ToolStripMenuItem1,
+            this.数据文件ToolStripMenuItem1});
+            this.打开配置ToolStripMenuItem.Name = "打开配置ToolStripMenuItem";
+            this.打开配置ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.打开配置ToolStripMenuItem.Text = "打开";
+            this.打开配置ToolStripMenuItem.Click += new System.EventHandler(this.打开配置ToolStripMenuItem_Click);
+            // 
+            // 配置文件ToolStripMenuItem1
+            // 
+            this.配置文件ToolStripMenuItem1.Name = "配置文件ToolStripMenuItem1";
+            this.配置文件ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.配置文件ToolStripMenuItem1.Text = "配置文件";
+            // 
+            // 数据文件ToolStripMenuItem1
+            // 
+            this.数据文件ToolStripMenuItem1.Name = "数据文件ToolStripMenuItem1";
+            this.数据文件ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.数据文件ToolStripMenuItem1.Text = "数据文件";
+            // 
+            // 保存配置ToolStripMenuItem
+            // 
+            this.保存配置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.配置文件ToolStripMenuItem,
+            this.数据文件ToolStripMenuItem});
+            this.保存配置ToolStripMenuItem.Name = "保存配置ToolStripMenuItem";
+            this.保存配置ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.保存配置ToolStripMenuItem.Text = "保存";
+            // 
+            // 配置文件ToolStripMenuItem
+            // 
+            this.配置文件ToolStripMenuItem.Name = "配置文件ToolStripMenuItem";
+            this.配置文件ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.配置文件ToolStripMenuItem.Text = "配置文件";
+            // 
+            // 数据文件ToolStripMenuItem
+            // 
+            this.数据文件ToolStripMenuItem.Name = "数据文件ToolStripMenuItem";
+            this.数据文件ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.数据文件ToolStripMenuItem.Text = "数据文件";
+            // 
+            // 保存数据ToolStripMenuItem
+            // 
+            this.保存数据ToolStripMenuItem.Name = "保存数据ToolStripMenuItem";
+            this.保存数据ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.保存数据ToolStripMenuItem.Text = "关闭";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.状态设置ToolStripMenuItem,
+            this.采样模式ToolStripMenuItem});
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 状态设置ToolStripMenuItem
+            // 
+            this.状态设置ToolStripMenuItem.Name = "状态设置ToolStripMenuItem";
+            this.状态设置ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.状态设置ToolStripMenuItem.Text = "状态设置";
+            // 
+            // 采样模式ToolStripMenuItem
+            // 
+            this.采样模式ToolStripMenuItem.Name = "采样模式ToolStripMenuItem";
+            this.采样模式ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.采样模式ToolStripMenuItem.Text = "采样模式";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(516, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(279, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 123);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 205);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(78, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(78, 51);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(79, 20);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "打开串口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "关闭串口";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "串口";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "波特率";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Location = new System.Drawing.Point(32, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(185, 123);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 258);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "串口测试程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 配置文件ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 数据文件ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 保存配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 配置文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存数据ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 状态设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 采样模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ C#/20170706/串口实验/串口实验/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+namespace 串口实验
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Text = "115200";//波特率默认值
+
+            button2.Enabled = true;//打开串口按钮可用
+            button3.Enabled = false;//关闭串口按钮不可用
+            /*****************非常重要************************/
+            string[] portList = System.IO.Ports.SerialPort.GetPortNames();
+            for (int i = 0; i < portList.Length; ++i)
+            {
+                string name = portList[i];
+                comboBox1.Items.Add(name);
+                comboBox1.Text = name;//串口号多额默认值
+            }
+            //serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);//必须手动添加事件处理程序
+
+        }
+
+        private void 打开配置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("时间到了!!!", "提示!!");//弹出提示框
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a;
+
+            a = textBox2.Text;
+            try//防止出错
+            {
+                //serialPort1.Open();
+                serialPort1.Write(a);
+                //serialPort1.Close();
+            }
+            catch
+            {//如果出错就执行此块代码
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();//如果是写数据时出错，此时窗口状态为开，就应关闭串口，防止下次不能使用，串口是不能重复打开和关闭的
+                    MessageBox.Show("端口错误", "串口已打开");
+                }
+                MessageBox.Show("端口错误", "错误");
+            }
+            //textBox1.AppendText(a);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string str = serialPort1.ReadExisting();
+            textBox1.AppendText(str);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();//关闭串口
+                button2.Enabled = true;//打开串口按钮可用
+                button3.Enabled = false;//关闭串口按钮不可用
+            }
+            catch (Exception err)//一般情况下关闭串口不会出错，所以不需要加处理程序
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text, 10);//十进制数据转换
+                serialPort1.DataBits = 8;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.Open();
+                button2.Enabled = false;//打开串口按钮不可用
+                button3.Enabled = true;//关闭串口
+            }
+            catch
+            {
+                MessageBox.Show("端口错误,请检查串口", "错误");
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
