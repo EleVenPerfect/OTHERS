232427fe9b5a5a069c720b6057a264b937bc6b8c
ATIME <atime@atime.org.cn>
2017-08-11 17:54:06 +0800

    usb

--- STM32F407/16.各类统计信息/13.8M晶振168MHzUSBDISK/User/BSP/usb/bsp_usbdisk.h
+++ STM32F407/16.各类统计信息/13.8M晶振168MHzUSBDISK/User/BSP/usb/bsp_usbdisk.h
@@ -4,7 +4,12 @@
 #include "stm32f4xx.h"
 #include "bsp_usart.h"
 
-
+signed char usb_disk_reset(void);
+signed char usb_disk_init(void);
+signed char usb_disk_creat_file	(unsigned char file_name[5]);
+signed char usb_disk_open_file	(unsigned char file_name[5]);
+signed char usb_disk_write_file	(unsigned char file_name[5]);
+signed char usb_disk_delete_file(unsigned char file_name[5]);
 
 #endif /* __USB_DISK_H */
 
