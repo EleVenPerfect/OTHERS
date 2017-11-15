2888d4eaee52956528a9bc253f2a382517f5cf95
ATIME <atime@atime.org.cn>
2017-11-15 15:15:11 +0800

    pycharm

---
+++ Python/pycharm/1.Spider/13.wyyyy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/15
+# @Author  : ATIME
+# @File    : 13.wyyyy.py
+
+def main():
+    pass
+
+
+    if __name__ == '__main__':
+        main()
