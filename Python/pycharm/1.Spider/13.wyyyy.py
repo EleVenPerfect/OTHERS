6cf633eaf8ce5bacd987cf57330aba0316ccb90c
ATIME <atime@atime.org.cn>
2017-11-19 13:58:02 +0800

    wyyyy

--- Python/pycharm/1.Spider/13.wyyyy.py
+++ Python/pycharm/1.Spider/13.wyyyy.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2017/11/15
+# encoding: utf-8
+# @Time    : 2017/11/18 22:36
 # @Author  : ATIME
+# @license : Apache Licence
+# @Contact : atime@atime.org.cn
+# @software: PyCharm
 # @File    : 13.wyyyy.py
+
 
 def main():
     pass
 
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
