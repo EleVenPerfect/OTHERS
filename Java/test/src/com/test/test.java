e391f7e05f851e39631cc00a9ca6779596e8dadd
ATIME <atime@atime.org.cn>
2018-11-26 10:21:58 +0800

    java

---
+++ Java/test/src/com/test/test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class test {
+
+	public static void main(String[] args) {
+
+		Collection c = new ArrayList<>();
+		c.add("dfdf"); 
+		System.out.println(c);
+	}
+
+}
